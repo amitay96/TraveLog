2f0f348aa62504d2b25719e64e915649f02db65c
amitay96 <amitay96@gmail.com>
2022-10-24 04:27:50 +0300

    feature: add crash test

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,12 @@ app.options("*", cors());
 app.use(reqLimiter);
 app.use(requestLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.post("/signin", login);
 app.post("/signup", validateAuth, createUser);
 
